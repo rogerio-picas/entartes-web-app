fd0f5885426925315eaf40d7c136a99573d4543a
JoseSousa <a31552@alunos.ipca.pt>
2026-04-11 01:01:56 +0100

    setup prisma with existing database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 const { neon } = require('@neondatabase/serverless');
 require('dotenv').config();
 
-// Inicializa a ligação usando a string de ligação guardada no .env
+// Inicializa a ligação usando a stringr de ligação guardada no .env
 const sql = neon(process.env.DATABASE_URL);
 
 module.exports = sql;
