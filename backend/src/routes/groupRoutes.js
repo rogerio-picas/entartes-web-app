65db83c4e1af26140ca01a1f4c28bbb21fbf3578
Rogério Picas <a18395@alunos.ipca.pt>
2026-04-18 11:19:02 +0100

    Events: Endpoints Update

--- backend/src/routes/groupRoutes.js
+++ backend/src/routes/groupRoutes.js
@@ -10,6 +10,14 @@ router.put("/grupos/:id_grupo", tokenValidation, authorize([1]), groupController
 // Eliminar: DELETE /api/event/grupos/delete/:id_grupo
 router.delete("/grupos/:id_grupo", tokenValidation, authorize([1]), groupController.eliminarGrupo);
 
+// --- Rotas genéricas com apenas um parâmetro (:id_evento) ---
+
+// Criar: POST /api/event/grupos/:id_evento
+router.post("/:id_evento/grupos", tokenValidation, authorize([1]), groupController.criarGrupo);
+
+// Listar: GET /api/event/grupos/:id_evento
+router.get("/:id_evento/grupos", tokenValidation, authorize([1,2,3]), groupController.listarGruposDoEvento);
+
 // --- Rotas de sub-recursos (Alunos/Docentes) ---
 
 // Alunos: POST /api/event/grupos/:id_grupo/alunos/:id_aluno
@@ -20,12 +28,5 @@ router.delete("/:id_evento/grupos/:id_grupo/alunos/:id_aluno", tokenValidation, 
 router.post("/:id_evento/grupos/:id_grupo/docentes/:id_docente", tokenValidation, authorize([1]), groupController.adicionarDocenteAoGrupo);
 router.delete("/:id_evento/grupos/:id_grupo/docentes/:id_docente", tokenValidation, authorize([1]), groupController.removerDocenteDoGrupo);
 
-// --- Rotas genéricas com apenas um parâmetro (:id_evento) ---
-
-// Criar: POST /api/event/grupos/:id_evento
-router.post("/:id_evento/grupos", tokenValidation, authorize([1]), groupController.criarGrupo);
-
-// Listar: GET /api/event/grupos/:id_evento
-router.get("/:id_evento/grupos", tokenValidation, authorize([1,2,3]), groupController.listarGruposDoEvento);
 
 module.exports = router;
