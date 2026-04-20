9c2706c5f905a529f056d3b7238c9d865e19dc91
hugo31545 <a31545@alunos.ipca.pt>
2026-04-20 15:01:36 +0100

    modificacao do home conforme tipo de utilizador

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,46 +2,64 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './views/Login'
 import Events from './views/Eventos'
 import Home from './views/Home'
+import HomeDocente from './views/HomeDocente'
+import HomeAdmin from './views/HomeAdmin'
 import Aulas from './views/Aulas'
 import Horario from './views/Horario'
 import Escola from './views/Escola'
 import Profile from './views/Profile'
-import HomeAdmin      from './views/HomeAdmin'
-import AulasAdmin     from './views/AulasAdmin'
-import Grupos         from './views/Grupos'
-import NovoEventoModal from './views/NovoEventoModal'
-
+import AulasAdmin from './views/AulasAdmin'
+import Grupos from './views/Grupos'
 import ProtectedRoute from './components/ProtectedRoute'
-import DashboardLayout from './components/DashboardLayout' 
+import RoleRoute from './components/RoleRoute'
+import DashboardLayout from './components/DashboardLayout'
+import { authService } from './services/authService'
+
+function HomeRedirect() {
+  const user = authService.getUser()
+  const role = user?.role ?? 3
+  if (role === 1) return <Navigate to="/admin/home" replace />
+  if (role === 2) return <Navigate to="/docente/home" replace />
+  return <Navigate to="/aluno/home" replace />
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rota Pública */}
         <Route path="/login" element={<Login />} />
-        
-        {/* Rotas Privadas com Header */}
-        <Route
-          element={
-            <ProtectedRoute>
-              <DashboardLayout />
-            </ProtectedRoute>
-          }
-        >
-          
-          {/* Todas as rotas abaixo herdam o Header automaticamente */}
-          <Route path="/"  element={<Home />} />  
-          <Route path="/home" element={<Navigate to="/" replace />} />
+
+        <Route element={
+          <ProtectedRoute><DashboardLayout /></ProtectedRoute>
+        }>
+          <Route path="/" element={<HomeRedirect />} />
+          <Route path="/home" element={<HomeRedirect />} />
+
+          {/* Coordenadora (role 1) */}
+          <Route path="/admin/home"
+            element={<RoleRoute roles={[1]}><HomeAdmin /></RoleRoute>} />
+          <Route path="/admin/aulas"
+            element={<RoleRoute roles={[1]}><AulasAdmin /></RoleRoute>} />
+
+          {/* Docente (role 2) */}
+          <Route path="/docente/home"
+            element={<RoleRoute roles={[2]}><HomeDocente /></RoleRoute>} />
+
+          {/* Aluno (role 3) */}
+          <Route path="/aluno/home"
+            element={<RoleRoute roles={[3]}><Home /></RoleRoute>} />
+
+          {/* Partilhadas */}
           <Route path="/horario" element={<Horario />} />
-          <Route path="/aulas" element={<Aulas  />} />
-          <Route path="/escola" element={<Escola />} />
+          <Route path="/aulas" element={<Aulas />} />
+          <Route path="/escola"
+            element={<RoleRoute roles={[1,2]}><Escola /></RoleRoute>} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/eventos" element={<Events />} />
-          <Route path="/grupos" element={<Grupos />} />
+          <Route path="/grupos"
+            element={<RoleRoute roles={[1]}><Grupos /></RoleRoute>} />
         </Route>
 
-        {/* Fallback para rotas inexistentes */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
