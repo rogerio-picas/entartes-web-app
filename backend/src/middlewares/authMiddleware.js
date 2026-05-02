92950d310b439c805acdbec96617cdb517554d8a
Matheus Delgado <matheus.delgado05@gmail.com>
2026-05-02 17:53:34 +0100

    Implementacao da verificacao de role na BD para revoke da JWT

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,22 +1,36 @@
 const jwt = require('jsonwebtoken');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-const tokenValidation = (req, res, next) =>{
+const tokenValidation = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if(!token) return res.status(401).json({ message: "Acesso negado. É necessária autenticação."});
+    if (!token) return res.status(401).json({ message: "Acesso negado. É necessária autenticação." });
 
-    try
-    {
+    try {
         const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+        //busca e compara na BD a role atual do utilizador com o id do token
+        const userReal = await prisma.utilizador.findUnique({
+            where: { id_utilizador: verfied.id },
+            select: { id_tipo: true }
+        });
+        //se o utilizador nao existir na BD:
+        if (!userReal) {
+            return res.status(401).json({ message: "Utilizador nao encontrado" });
+        }
+        //se a role da BD for diferente da role do token: 
+        if (userReal.id_tipo !== verified.role) {
+            return res.status(401).json({ message: "Permissões alteradas. Faça login novamente." });
+        }
 
         req.user = verified;
 
         next();
     }
-    catch(error)
-    {
-        res.status(401).json({ message: "Token inválido ou sessão expirada.", error: error.message});
+    catch (error) {
+        res.status(401).json({ message: "Token inválido ou sessão expirada.", error: error.message });
     }
 };
 
