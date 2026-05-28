6c289fd1e2c5f5877fb0fd23764b0f9c944cd02e
ivofernandes8819 <a8819@alunos.ipca.pt>
2026-05-28 11:04:16 +0100

    remove hardcoded url

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 export const authService = {
   async login(codigo_username, password) {
-    const response = await fetch('http://localhost:3000/api/auth/login', {
+    const response = await fetch('/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ codigo_username, password }),
